ab9da7fd4bd1d0d2e67789ce5574a17bb31505f9
SamTusick <stusick@outlook.com>
2025-05-20 16:53:37 -0400

    Started About Page heading and base structure

--- portfolio-site/src/pages/About.jsx
+++ portfolio-site/src/pages/About.jsx
@@ -1,4 +1,14 @@
 /* About Page */
+import "../styling/about.css"
+
 export default function About(){
-    return <h1>About</h1>
+    return (
+        <section className="about-section">
+            <div className="about-header-section">
+                <h3 className="about-header-top">Get to Know More</h3>
+                <h2 className="about-header-bottom">About Me</h2>
+            </div>
+
+        </section>
+    );
 }
