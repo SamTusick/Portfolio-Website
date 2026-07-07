80b2c05d7c708df45b921c3572812f974de5ce2c
SamTusick <stusick@outlook.com>
2026-07-06 21:43:42 -0400

    Added home page image

--- portfolio-site/src/pages/Home.jsx
+++ portfolio-site/src/pages/Home.jsx
@@ -1,28 +1,36 @@
 /* Home Page */
 import "../styling/home.css";
 import { Typewriter } from 'react-simple-typewriter';
+import panelPic from "../assets/Panel_pic.JPEG";
 
-export default function Home() {
-  return (
-    <section className="home-section">
-      <div className="home-intro-group">
-        <h1 className="home-intro">Hello, I'm</h1>
-        <h1 className="home-name">Samuel Tusick</h1>
-      </div>
-      <h2 className="typewriter-text">
-        <span className="home-typewriter-static">I'm&nbsp;</span>
-        <span className="home-typewriter-dynamic">
-          <Typewriter
-            words={['An Aspiring Software Engineer', 'An Agentic AI Researcher', 'A Full-Stack Developer']}
-            loop={true}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1500}
-          />
-        </span>
-      </h2>
-    </section>
-  );
-}
+<section className="home-section">
+  <div className="home-content">
+    <div className="home-intro-group">
+      <h1 className="home-intro">Hello, I'm</h1>
+      <h1 className="home-name">Samuel Tusick</h1>
+    </div>
+
+    <h2 className="typewriter-text">
+      <span className="home-typewriter-static">I'm&nbsp;</span>
+      <span className="home-typewriter-dynamic">
+        <Typewriter
+          words={[
+            'An Aspiring Software Engineer',
+            'An Agentic AI Researcher',
+            'A Full-Stack Developer'
+          ]}
+          loop
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1500}
+        />
+      </span>
+    </h2>
+  </div>
+
+  <div className="home-image">
+    <img src={panelPic} alt="Picture of Samuel Tusick" />
+  </div>
+</section>
